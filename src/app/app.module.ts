872b510b7e19cbed26ca9e35d3c11d86690525d0
alan oña <afona2@espe.edu.ec>
2022-06-04 01:21:55 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { CargarScriptsService } from "./servicios/cargar-scripts.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    CargarScriptsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
